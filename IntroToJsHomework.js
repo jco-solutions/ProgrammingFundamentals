649e319fea26eef0f382b059869fe9cd76dea147
Jennifer Combs <jco-solutions@outlook.com>
2020-10-28 19:50:33 -0400

    tuesday js homework update

--- IntroToJsHomework.js
+++ IntroToJsHomework.js
@@ -25,3 +25,27 @@ function hello(name) {
   }
   tipAmount(100, "good")
   tipAmount(40, "fair")
+
+  function totalAmount(amount, rating){
+    if (rating === "good") {
+        console.log((amount * .2) + amount);
+    } else if (rating === "fair") {
+        console.log((amount *.15) + amount);
+    } else {
+        console.log((amount * .1) + amount);
+    }
+}
+totalAmount (100, "good")
+totalAmount (40, "fair")
+
+function splitAmount(amount, rating, population){
+    if (rating === "good") {
+        console.log(((amount * .2) + amount) / population);
+    } else if (rating === "fair") {
+        console.log(((amount *.15) + amount) / population);
+    } else {
+        console.log(((amount * .1) + amount) / population);
+    }
+}
+splitAmount (100, "good", 5)
+splitAmount (40, "fair", 2)
